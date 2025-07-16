406137c0386deb6910d68de88f9046b4f137801f
Vardaan Madhisiya <madhisiya.vardan24@gmail.com>
2025-07-17 03:13:49 +0530

    First_Commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import ThemeToggle from "../components/ThemeToggle";
+import StarBackground from "../components/StarBackground";
+
+const Home = () => {
+    return <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
+
+        {/* Theme Toggle */}
+        <ThemeToggle />
+
+        {/* Background Effects */}
+        <StarBackground />
+
+        {/* Navbar */}
+
+        {/* Main Content */}
+
+        {/* Footer */}
+    </div>;
+};
+
+export default Home;
