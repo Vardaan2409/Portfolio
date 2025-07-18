2376dd7059d1974200f6af7c3673a075e0dad3e8
Vardaan Madhisiya <madhisiya.vardan24@gmail.com>
2025-07-18 17:29:35 +0530

    AboutSection completed

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import ThemeToggle from "../components/ThemeToggle";
 import StarBackground from "../components/StarBackground";
 import Navbar from "../components/Navbar";
+import HeroSection from "../components/HeroSection";
+import AboutSection from "../components/AboutSection";
 
 const Home = () => {
     return <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
@@ -16,6 +18,8 @@ const Home = () => {
         <Navbar />
 
         {/* Main Content */}
+        <HeroSection />
+        <AboutSection />
 
         {/* Footer */}
     </div>;
