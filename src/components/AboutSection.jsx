3f165aadb4d301e668918c670951ea1649343f08
Vardaan Madhisiya <madhisiya.vardan24@gmail.com>
2025-07-20 17:24:26 +0530

    changes

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -37,7 +37,11 @@ const AboutSection = () => {
                                 Get In Touch
                             </a>
 
-                            <a href='' className='px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300'>
+                            <a 
+                                href='/Vardaan_Madhisiya_CV.pdf' 
+                                download
+                                className='px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300'
+                            >
                                 {" "}
                                 Download Cv
                             </a>
